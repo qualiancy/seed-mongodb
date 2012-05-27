c6365c8bcff649b601a068ff3165f09fa0295d59
Jake Luer <jake.luer@gmail.com>
2012-05-27 01:48:16 -0400

    tests for model using schema

--- test/model.js
+++ test/model.js
@@ -19,7 +19,10 @@ describe('MongoStore being used in the MODEL context', function () {
   });
 
   var arthur = new Person({
-    name: 'Arthur Dent'
+      name: 'Arthur Dent'
+    , meta: {
+        species: 'Human'
+      }
   });
 
   before(store.connect.bind(store));
@@ -46,6 +49,7 @@ describe('MongoStore being used in the MODEL context', function () {
     dent.fetch(function (err) {
       should.not.exist(err);
       dent.get('name').should.equal(arthur.get('name'));
+      dent.get('meta.species').should.equal(arthur.get('meta.species'));
       done();
     });
   });
@@ -60,6 +64,7 @@ describe('MongoStore being used in the MODEL context', function () {
       confirm.fetch(function (err) {
         should.not.exist(err);
         confirm.get('location').should.equal(arthur.get('location'));
+        confirm.get('meta.species').should.equal(arthur.get('meta.species'));
         done();
       });
     });
@@ -83,3 +88,94 @@ describe('MongoStore being used in the MODEL context', function () {
 
 });
 
+describe('MongoStore in the MODEL context with a SCHEMA', function () {
+  var store = new MongoStore(testopts);
+
+  var PersonSchema = new Seed.Schema({
+      _id: {
+          type: String
+        , index: true
+      }
+
+    , name: String
+
+    , location: String
+
+    , meta: {
+        species: String
+      }
+  });
+
+  var Person = Seed.Model.extend('person', {
+      store: store
+    , schema: PersonSchema
+  });
+
+  var arthur = new Person({
+      name: 'Arthur Dent'
+    , meta: {
+        species: 'Human'
+      }
+  });
+
+  before(store.connect.bind(store));
+  after(function (done) {
+    store.db.dropDatabase();
+    store.close(done);
+  });
+
+
+  var id;
+  it('should allow a new object to be created', function (done) {
+    arthur.save(function (err) {
+      id = arthur.get('_id');
+      should.not.exist(err);
+      done();
+    });
+  });
+
+  it('should allow an already writtin object to be retrieved', function (done) {
+    var dent = new Person({
+      _id: id
+    });
+
+    dent.fetch(function (err) {
+      should.not.exist(err);
+      dent.get('name').should.equal(arthur.get('name'));
+      dent.get('meta.species').should.equal(arthur.get('meta.species'));
+      done();
+    });
+  });
+
+  it('should allow an already written object to be modified', function (done) {
+    arthur.set('location', 'earth');
+    arthur.save(function (err) {
+      should.not.exist(err);
+      var confirm = new Person({
+          _id: id
+      });
+      confirm.fetch(function (err) {
+        should.not.exist(err);
+        confirm.get('location').should.equal(arthur.get('location'));
+        confirm.get('meta.species').should.equal(arthur.get('meta.species'));
+        done();
+      });
+    });
+  });
+
+  it('should allow for an already existing item to be removed', function (done) {
+    arthur.destroy(function (err) {
+      should.not.exist(err);
+      var confirm = new Person({
+        _id: id
+      });
+
+      confirm.fetch(function (err) {
+        should.exist(err);
+        err.should.be.instanceof(Seed.SeedError);
+        err.code.should.equal('ENOTFOUND');
+        done();
+      });
+    });
+  });
+});
